b466a8c42423199db204fc15e1f1e7fd1c0f22f1
Mani Deepak T <manideepak0071@gmail.com>
2022-10-04 17:20:06 +0300

    variables in log file

---
+++ 03-vars.sh
@@ -0,0 +1,16 @@
+a=10   #integer
+b=abc  #string
+c=true #boolean
+
+echo value of a = $a
+
+# Command Subs
+DATE=$(date)
+echo Today Date is $SATE
+
+# Arthimetic Subs
+ADD=$((2+3+4))
+echo Addition of 2+3+4 is $ADD
+
+## Variables from CLI
+echo Value of x = $x
