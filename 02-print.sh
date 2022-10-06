39d7b7f596d53d6dd187ce0a430a6ad82029038c
Mani Deepak T <manideepak0071@gmail.com>
2022-10-06 16:16:22 +0300

    If conditions on project

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,17 @@
 echo Hello World
+
+## Color print syntax
+# echo -e "\e{COLmMESSAGE\e[0m"
+# Ex: echo -e "\e[31mHELLO\e[0m"
+# \e[31m -> To enable color code 31
+# \e[0m -> To disabled the enabled color we use 0
+# -e -> Enable esc seq, \e is one esc seq
+#"" -> Quotes are mandatory if we useesc seq
+
+### Colors
+# RED       31
+# GREEN     32
+# YELLOW    33
+# BLUE      34
+# MAGENTA   35
+# CYAN      36
