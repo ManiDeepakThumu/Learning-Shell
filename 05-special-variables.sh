9638a62fbaedf4db51bbc8f58978b726ec33c46b
Mani Deepak T <manideepak0071@gmail.com>
2022-10-06 10:46:28 +0300

    MongoDB

--- 05-special-variables.sh
+++ 05-special-variables.sh
@@ -1,8 +1,7 @@
 
-
-echo $0
-echo $1
-echo $2
-echo $*
-echo $@
-echo $#
+echo $0 # Script Name
+echo $1 # First Argument
+echo $2 # Second Argument
+echo $* # All Arguments
+echo $@ # All Arguments
+echo $# # No of Arguments
